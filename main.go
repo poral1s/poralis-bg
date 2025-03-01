e9f2abba7940ad531e5c8cec85b1df6954606c6e
Hayabusa58 <hayabusa3430@gmail.com>
2025-03-02 03:10:54 +0900

    CORS 対応

--- main.go
+++ main.go
@@ -6,12 +6,15 @@ import (
 	"github.com/Hayabusa58/polar1s/polaris-bg/db"
 	"github.com/Hayabusa58/polar1s/polaris-bg/handlers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	db.Init()
 
 	app := fiber.New()
+	app.Use(cors.New())
+
 	handlers.Init(app)
 
 	log.Println(app.Listen(":3000"))
